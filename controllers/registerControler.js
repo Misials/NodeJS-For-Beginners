0693d05f28ca79eccc06e1712f44fd1f2474346f
Misials <michalski.mikolaj10@wp.pl>
2022-09-25 07:32:40 +0200

    Chapter 14 - Mongoose Data Models. Finished

--- controllers/registerControler.js
+++ controllers/registerControler.js
@@ -1,14 +1,5 @@
 const bcrypt = require('bcrypt');
-const path = require('path');
-const fsPromises = require('fs').promises;
-const users = require('../model/users.json');
-
-const usersDB = {
-	users: users,
-	setUsers: function (data) {
-		this.users = data;
-	},
-};
+const User = require('./../model/userModel');
 
 exports.handleNewUser = async (req, res) => {
 	const { user, pwd } = req.body;
@@ -16,22 +7,21 @@ exports.handleNewUser = async (req, res) => {
 	if (!user || !pwd) {
 		return res.status(400).json({ message: 'Username and password are required!' });
 	}
+
 	// Check if username is not duplicated
-	const duplicate = usersDB.users.find(usr => usr.username === user);
+	const duplicate = await User.findOne({ username: user }).exec();
 	if (duplicate) return res.sendStatus(409);
 	try {
 		// Encrypt password
 		const hashedPwd = await bcrypt.hash(pwd, 10);
-		// Store new user
-		const newUser = {
+		// Create and store new user
+		const result = await User.create({
 			username: user,
-			roles: {
-				User: 2001,
-			},
 			password: hashedPwd,
-		};
-		usersDB.setUsers([...usersDB.users, newUser]);
-		await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(usersDB.users));
+		});
+
+		console.log(result);
+
 		res.status(201).json({ message: `New user ${user} was created!` });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
